674f5fe6381b3ca8d6f0f6fa1ffb59bec88e3d43
markmumba <markmumba01@gmail.com>
2019-09-01 12:10:13 +0300

    added

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile (models.Model):
+    profile_photo =models.ImageField(upload_to = 'profiles/',null = True)
+    user_bio = models.TextField()
+    user =models.ForeignKey(User)
+    last_update=models.DateTimeField(auto_now_add=True, null=True)
+
+
+    class Meta :
+        ordering = ['-last_update']
+
+
+    def __str__ (self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+        
+
