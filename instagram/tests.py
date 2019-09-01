bcf71906b04f5ea521e674b50cdafc276e148d29
markmumba <markmumba01@gmail.com>
2019-09-01 19:06:42 +0300

    tests

--- instagram/tests.py
+++ instagram/tests.py
@@ -2,4 +2,13 @@ from django.test import TestCase
 import datetime as dt 
 from .models import Image, Profile
 from django.contrib.auth.models import User
+
+
+
+
+
 # Create your tests here.
+
+class ImageTestClass(TestCase):
+    
+
