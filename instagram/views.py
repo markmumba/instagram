b2815a84df7bb3cbe2175ed8812b8c6670c9c9c8
markmumba <markmumba01@gmail.com>
2019-09-01 15:40:54 +0300

    finished with view function

--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .models import Image,Comments,Profile
 from .forms import NewCommentForm,NewsStatusForm
 
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def timelines(request):
     current_user = request.user
     images = Image.objects.order_by('-date_uploaded')
@@ -12,12 +13,18 @@ def timelines(request):
 
     return render(request,'timelines.html',{'images':images ,'profiles':profiles,'user_profile':user_profile,'comments':comments})
 
+
+
+@login_required(login_url='/accounts/login/')
 def profile(request):
     current_user = request.user
     profile = Profile.objects.get(user_id = current_user.id)
     images = Image.objects.all().filter(profile_id = current_user.id)
     return render(request,'profile.html',{'images':images,'profile':profile})
 
+
+
+@login_required(login_url='/accounts/login/')
 def new_status(request, username):
     current_user = request.user
     username = current_user.username
@@ -37,3 +44,42 @@ def user_profile(request,user_id):
     profile = Profile.objects.get(id=user_id)
     images = Image.objects.all().filter(user_id=user_id)
     return render(request, 'profile.html', {'profile':profile, 'images':images})
+
+
+
+@login_required(login_url='/accounts/login')
+def single_image(request, photo_id):
+    image = Image.objects.get(id = photo_id)
+    return render(request,'single_image.html',{'image':image})
+
+def find_profile(request):
+    if 'images' in request.GET and request.GET['images']:
+        search_term = request.GET.get('images')
+        searched_image = Image.search_by_user(search_term)
+        return render(request, 'user_profile.html', {'images':searched_image})
+    else:
+        message = 'You haven\'t searched for anything'
+        return render(request, 'single_image.html')    
+
+
+@login_required (login_url='/accounts/register/')
+def single_image_like(request, photo_id):
+    image = Image.objects.get(id=photo_id)
+    image.likes = image.likes + 1
+    image.save()
+    return redirect('allTimelines')
+
+@login_required(login_url='/accounts/login/')
+def new_comment(request, username):
+    current_user = request.user
+    username = current_user.username
+    if request.method == 'POST':
+        form = NewCommentForm(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save()
+            comment.user = request.user
+            comment.save()
+        return redirect('allTimelines')
+    else:
+        form = NewCommentForm()
+    return render(request, 'new_comment.html', {"form": form})
